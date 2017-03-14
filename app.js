49e34dacaa3da21f2c6f0a23ddfe9c06c86296cd
YanJing <564752135@qq.com>
2017-03-14 16:03:40 +0800

    finish day 5

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ var tpl = heredoc(function () {/*
         <body>
             <h1>点击标题, 开始录音翻译</h1>
             <p id="title"></p>
+            <div id="year"></div>
+            <div id="director"></div>
             <div id="poster"></div>
             <script src="http://zeptojs.com/zepto.min.js"></script>
             <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
@@ -39,6 +41,12 @@ var tpl = heredoc(function () {/*
                      nonceStr: '<%= noncestr%>', // 必填，生成签名的随机串
                      signature: '<%= signature%>',// 必填，签名，见附录1
                      jsApiList: [
+                             'onMenuShareTimeline',
+                             'onMenuShareAppMessage',
+                             'onMenuShareQQ',
+                             'onMenuShareWeibo',
+                             'onMenuShareQZone',
+                             'previewImage',
                              'startRecord',
                              'stopRecord',
                              'onVoiceRecordEnd',
@@ -54,7 +62,27 @@ var tpl = heredoc(function () {/*
                          }
                      })
 
+                     var shareContent = {
+                         title: 'sousousou', // 分享标题
+                         desc: '我搜出来了啥', // 分享描述
+                         link: 'https://github.com', // 分享链接
+                         imgUrl: 'http://static.mukewang.com/static/img/common/logo.png', // 分享图标
+                         success: function () {
+                            window.alert('分享成功')
+                         },
+                         cancel: function () {
+                            window.alert('分享失败')
+                         }
+                     }
+                     wx.onMenuShareAppMessage(shareContent)
+
                      var isRecording = false
+                     var slides
+                     $('#poster').on('tap', function(){
+                         wx.previewImage(slides)
+                     }
+
+
                      $('h1').on('tap', function(){
                         if(!isRecording){
                            isRecording = true
@@ -62,6 +90,7 @@ var tpl = heredoc(function () {/*
                              cancel:function(){
                                 window.alert('搜索不了啦,请给我权限撒^_^')
                              }
+
                            })
                            return
                         }
@@ -69,19 +98,54 @@ var tpl = heredoc(function () {/*
                          isRecording = false
 
                          wx.stopRecord({
-                         success: function (res) {
-                             var localId = res.localId
+                             success: function (res) {
+                                 var localId = res.localId
 
-                             wx.translateVoice({
-                                 localId: localId, // 需要识别的音频的本地Id，由录音相关接口获得
-                                 isShowProgressTips: 1, // 默认为1，显示进度提示
-                                 success: function (res) {
-                                    window.alert(res.translateResult); // 语音识别的结果
-                                 }
-                             })
+                                 wx.translateVoice({
+                                     localId: localId, // 需要识别的音频的本地Id，由录音相关接口获得
+                                     isShowProgressTips: 1, // 默认为1，显示进度提示
+                                     success: function (res) {
+                                        var result = res.translateResult // 语音识别的结果
+                                        // window.alert(result)
+                                        $.ajax({
+                                            type:'get',
+                                            url: 'https://api.douban.com/v2/movie/search?q=' + result,
+                                            dataType: 'jsonp',
+                                            jsonp:'callback',
+                                            success: function(data){
+                                                var subject = data.subjects[0]
+                                                $('#title').html(subject.title)
+                                                $('#year').html(subject.year)
+                                                $('#director').html(subject.directors[0].name)
+                                                $('#poster').html('<img src="' + subject.images.large + '"/>')
 
+                                                shareContent = {
+                                                     title: subject.title, // 分享标题
+                                                     desc: '搜索结果' + subject.title, // 分享描述
+                                                     link: 'https://github.com', // 分享链接
+                                                     imgUrl: subject.images.large, // 分享图标
+                                                     success: function () {
+                                                        window.alert('分享成功')
+                                                     },
+                                                     cancel: function () {
+                                                        window.alert('分享失败')
+                                                     }
+                                               }
 
+                                               slides = {
+                                                    current: subject.images.large,
+                                                    urls:[subject.images.large],
+                                               }
 
+                                               data.subjects.forEach(function(item){
+                                                    slides.urls.push(item.images.large)
+                                               })
+
+                                               wx.onMenuShareAppMessage(shareContent)
+                                            }
+                                        })
+                                     }
+                                 })
                              }
                          })
                      })
